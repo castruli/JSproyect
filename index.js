8d93e6271ce183b416ca803802e67b7a093f0c56
Facundo Castro <facucastro11@gmail.com>
2023-08-06 21:14:42 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,6 @@
+let entrada = prompt ("Quien ganará estas elecciones?");
+const nombre = prompt ("Ingrese su nombre");
+let salida = entrada + " " + "triunfará";
+alert (salida)
+
+const consulta = console.log ("nombre")
