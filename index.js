3d8c310cdbf5938cb9ad14d2a997e42081fcc1be
Facundo Castro <facucastro11@gmail.com>
2023-08-12 23:00:22 -0300

    primer guardado JS

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-// let entrada = prompt ("Quien ganará estas elecciones?");
-// const nombre = prompt ("Ingrese su nombre");
-// let salida = entrada + " " + "triunfará";
-// alert (salida)
+// mensaje bienvenida
+alert ("Bienvenido!")
+// cantidad de usuarios
+const totalParticipantes = parseInt(prompt ("¿Cuantos participantes serán?"))
 
-// const consulta = console.log ("nombre")
+// registracion de usuarios
+
+for ( let participante = 1; participante <= totalParticipantes; participante++ ){
+    const nombreParticipante = prompt("Ingrese participante")
+    alert ("Participante N°" + participante + "- Nombre: " + nombreParticipante)
+    console.log (nombreParticipante)
+}
+alert("Carga de participantes completa")
+
+// ingreso de gastos
+alert('A continuación podrá ingresar los siguientes conceptos:')
+alert('Alojamiento, Traslado, Salidas, Dia e Higiene')
+
+function concepto() {
+    const conceptoAsignado = prompt('que categoria')
+    console.log(conceptoAsignado)    
+}
+concepto()
+
+let cantidadDeComprobantes = parseInt(prompt('ingrese la cantidad de comprobantes a cargar'))
+while (isNaN(cantidadDeComprobantes)){
+    cantidadDeComprobantes = prompt( 'ingrese un monto válido')
+}
+
