7a4c7cc21a7e8b263d3430bcabcd09569ccb8cbe
Facundo Castro <facucastro11@gmail.com>
2023-08-30 21:41:36 -0300

    incorporando map

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// mensaje bienvenida
+
 function start () {
     alert ('Bienvenido')
 }
@@ -6,13 +6,12 @@ function completed (){
     alert ('¡Carga completa!')
 }
 
-
 function premium (){
     alert ('Opción disponible para usuarios premium')
 }
 
 function seleccCategoria (){
-    alert ('Seleciona una catoria:'+ '\n' + 'Alojamiento' + '\n' + 'Traslado' + '\n' + 'Salidas' + '\n' + 'Dia a Dia' + '\n' + 'Higiene')
+    alert ('Seleciona una catoria:'+ '\n' + 'Alojamiento' + '\n' + 'Traslado' + '\n' + 'Salidas' + '\n' + 'Dia a Dia' + '\n' + 'Higiene' + '\n' + 'Otro')
 }
 const tiposCategorias = {
     alojamiento : 'Alojamiento',
@@ -20,8 +19,9 @@ const tiposCategorias = {
     salidas :'Salidas',
     dia : 'Dia a Dia',
     higiene: 'Higiene',
+    otro: 'Otro'
 }
-
+//agregar al seleccionar otro msj de premium
 
 function numeroDeParticipantes (){
     cantParticipantes = parseInt(prompt('¿Cuantos participantes serán?'))
@@ -63,17 +63,17 @@ if (cantParticipantes < 10){
   let numeroDeComprobantes = parseInt(prompt('Ingrese cantidad de comprobantes a registrar'))
   while (isNaN(numeroDeComprobantes)){
     numeroDeComprobantes = prompt( 'ingrese un monto válido')
-}
+    }
 
 
-class ingresoComprobantes {
+    class ingresoComprobantes {
     constructor (categoria,fecha,importe,abonado){
         this.categoria = categoria
         this.fecha = fecha
         this.importe = importe
         this.abonado = abonado
     }
-}
+    }
     const datosComprobantes = []
     for (let i = 0; i < numeroDeComprobantes; i++) {
       const categoria = categorias
@@ -86,82 +86,20 @@ class ingresoComprobantes {
     }
     console.log (datosComprobantes)
 
+    let totalImporte = 0;
+    const importesCategoria = datosComprobantes.map(v => v.importe) 
 
+    for (const i of importesCategoria) { 
+    totalImporte = totalImporte + i 
+    }
+    console.log(totalImporte)
 
-
-let cantidadDeComprobantes = parseInt(prompt('ingrese la cantidad de comprobantes a cargar'))
-console.log (cantidadDeComprobantes)
-
-
-
-
-//     for ( let comprobantes = 0; comprobantes<cantidadDeComprobantes; comprobantes++){
-//         let comprobantes = parseFloat(prompt('Ingrese monto del comprobante'))
-//         console.log (comprobantes)
-//         alert('¡Carga exitosa!')
-//     }
-//     alert ('Muchas gracias')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    alert ('El total ingresado es de $' + ' ' + totalImporte + ' en la categoria' + ' ' + categorias)
 
 }
 else {
      premium ()
 }
-
-
-
-
-
-// function concepto() {
-//     const conceptoAsignado = parseInt(prompt('Ingrese una categoria'))
-//     console.log(conceptoAsignado)    
-//     }
-
-// if (totalParticipantes < 10){
-//     for ( let participante = 1; participante <= totalParticipantes; participante++ ){
-//         const datosParticipantes = [];
-
-//         const nombre = prompt("Ingrese Nombre y Apellido")
-//         const mail = promt('Ingrese Mail')
-//         const banco = promt('Ingrese Banco')
-//         const cbu = prompt ('Ingrese CBU')
-
-
-
-
-//         alert ("Participante N°" + participante + "- Nombre: " + nombreParticipante)
-//         console.log (nombreParticipante)
-//     }
-//     //a utilizar en el futuro para el ingreso
-    
-//     for (const datos of datosParticipantes) {
-//     console.log (datos) 
-//     }
-
-
-//     alert("Carga de participantes completa")
-
-//     // ingreso de gastos
-//     alert('A continuación podrá ingresar los siguientes conceptos:')
-//     alert('01 - Alojamiento, 02 - Traslado, 03 - Salidas, 04 - Dia, 05 - Higiene')
- 
-//     concepto()
-  
 
 
 
