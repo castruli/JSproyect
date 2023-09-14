cf0c95a22e93aef056d67d1ffd81906a6195c78e
Facundo Castro <facucastro11@gmail.com>
2023-09-14 05:07:11 -0300

    modificando estructura JS

--- JS/dom.js
+++ JS/dom.js
@@ -1,14 +1,3 @@
-const ingresoFecha = document.getElementById('ingresoFecha')
-const ingresoImporte = document.getElementById('ingresoImporte')
-const ingresoCategoria = document.getElementById('ingresoCategoria') 
-const seleccionUsuario = document.getElementById('seleccionUsuario')
-const fechaTable = document.getElementById ('fechaTable')
-const ingresoTable = document.getElementById ('ingresoTable') 
-const categoriaTable = document.getElementById ('categoriaTable')
-const usuarioTable = document.getElementById ('usuarioTable')
-const alertComprobantes = document.getElementById('alertComprobantes')
-const formComprobante = document.getElementById('ingresoComprobante')
-
 class ingresoComprobantes {
     constructor (fecha, importe, categoria,usuario){
         this.fecha = fecha
@@ -18,23 +7,47 @@ class ingresoComprobantes {
     }
 }
 
+
+
+// DOM CARGA DATOS POR EL USUARIO //
+const formComprobante = document.getElementById('ingresoComprobante')
+const ingresoFecha = document.getElementById('ingresoFecha')
+const ingresoImporte = document.getElementById('ingresoImporte')
+const ingresoCategoria = document.getElementById('ingresoCategoria') 
+const seleccionUsuario = document.getElementById('seleccionUsuario')
+
+
+//DOM SALIDA DATOS DEL USUARIO //
+// const fechaTable = document.getElementById ('fechaTable')
+// const ingresoTable = document.getElementById ('ingresoTable') 
+// const categoriaTable = document.getElementById ('categoriaTable')
+// const usuarioTable = document.getElementById ('usuarioTable')
+const bodyTable = document.getElementById('bodyTable')
+const alertComprobantes = document.getElementById('alertComprobantes')
+
+
+
+
 // REVISAR ESTA FUNCION. AL ACTUALZIAR PAGINA NO VUELVE A TRAER DATOS, DUPLICA EL INGRESO EN CADA LINEA, NO GUARDA ARRAY
+
+
 let datosComprobantes = []
-const jsonComprobantes = localStorage.getItem('datosComprobantes')
+const jsonComprobantes = localStorage.getItem('comprobantes')
     if (jsonComprobantes){
         datosComprobantes = JSON.parse(jsonComprobantes)
     }
-agregarComprobante ()
+    agregarComprobante ()
 
 
-formComprobante.addEventListener('submit', e => {
+
+formComprobante.addEventListener('submit', (e) => {
 e.preventDefault()
 const comprobante = new ingresoComprobantes (ingresoFecha.value, ingresoImporte.value, ingresoCategoria.value, seleccionUsuario.value)
 datosComprobantes.push(comprobante)
 localStorage.setItem('comprobantes', JSON.stringify(datosComprobantes))
 agregarComprobante ()
 
-ingresoFecha.value = '';
+      ingresoFecha.value = '';
       ingresoImporte.value = '';
       ingresoCategoria.value = '';
       seleccionUsuario.value = '';
@@ -44,20 +57,37 @@ ingresoFecha.value = '';
 
 function agregarComprobante (){
     const newComprobante = document.createElement("tr");
+
+   
     const fechaTable = document.createElement("td");
-    fechaTable.innerHTML = datosComprobantes.map( f => f.fecha)
     const ingresoTable = document.createElement("td");
-    ingresoTable.innerHTML = datosComprobantes.map( i => i.importe)
     const categoriaTable = document.createElement("td");
-    categoriaTable.innerHTML = datosComprobantes.map( c => c.categoria)
     const usuarioTable = document.createElement("td");
-    usuarioTable.innerHTML = datosComprobantes.map( u => u.usuario)
+
+    const bFechaTable = datosComprobantes.map( f => f.fecha)
+    const bIngresoTable = datosComprobantes.map( i => i.importe) 
+    const bCategoriaTable = datosComprobantes.map( c => c.categoria)
+    const bUsuarioTable = datosComprobantes.map( u => u.usuario)
+   
+    fechaTable.innerHTML = ''
+    ingresoTable.innerHTML = ''
+    categoriaTable.innerHTML = ''
+    usuarioTable.innerHTML = ''
+
+    fechaTable.innerHTML = bFechaTable
+    ingresoTable.innerHTML = bIngresoTable 
+    categoriaTable.innerHTML = bCategoriaTable
+    usuarioTable.innerHTML = bUsuarioTable
+
 
     newComprobante.appendChild(fechaTable);
     newComprobante.appendChild(ingresoTable);
     newComprobante.appendChild(categoriaTable);
     newComprobante.appendChild(usuarioTable);
-    bodytable.appendChild(newComprobante);
+
+   
+    bodyTable.appendChild(newComprobante);    
+
 
     if (datosComprobantes.length > 0) {
         alertComprobantes.style.display = "none";
@@ -65,14 +95,16 @@ function agregarComprobante (){
         alertComprobantes.style.display = "block";
       }
     
-      
 
 }
 
-    const fecha = ingresoFecha.value;    
-    const importe = ingresoImporte.value;
-    const categoria = ingresoCategoria.value;
-    const usuario = seleccionUsuario.value;
 
 
 
+    // const fecha = ingresoFecha.value;    
+    // const importe = ingresoImporte.value;
+    // const categoria = ingresoCategoria.value;
+    // const usuario = seleccionUsuario.value;
+
+
+console.log(datosComprobantes)
