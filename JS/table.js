4a22857bd3ab1b8cd891b023c987a4b07206aa41
Facundo Castro <facucastro11@gmail.com>
2023-09-27 22:02:25 -0300

    modificaciones funciones table

--- JS/table.js
+++ JS/table.js
@@ -1,12 +1,36 @@
-// BUSQUEDA POR COMPROBANTE
-const cate = ''
+// BUSQUEDA POR COMPROBANTE + IMPORTE TOTAL
+
 function busquedaCategoria (cate){
+    let total = 0;
     const cateBuscada = datosComprobantes.filter( c => c.categoria === cate);
+    const importesCategoria = cateBuscada.map(v => parseFloat(v.importe)) 
+    importesCategoria.forEach(p => { total = total + p })
     console.log(cateBuscada)
+    console.log(total)   
 }
+
+
 busquedaCategoria ('Alojamiento');
 busquedaCategoria ('Traslado');
 busquedaCategoria ('Higiene');
 busquedaCategoria ('Dia');
 busquedaCategoria ('Salidas');
 busquedaCategoria ('Otros');
+
+// BUSQUEDA POR USUARIO 
+
+
+
+
+// crear for of o simil que me permita iterar con todos los posibles usuarios que
+// haya y calcular el total pagado por cada uno de ellos 
+function busquedaUsuario (us){
+    let total = 0;
+    const usuarioBuscado = datosComprobantes.filter( u => u.usuario === us);
+    const importesUsuarios = usuarioBuscado.map(v => parseFloat(v.importe)) 
+    importesUsuarios.forEach(p => { total = total + p })
+    console.log(total)   
+    console.log(usuarioBuscado)
+}
+
+busquedaUsuario('Facundo')
